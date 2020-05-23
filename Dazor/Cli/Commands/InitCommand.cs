843e37505534dd3d8a11a654f8d73f3283c5d556
Josh Olson <Olson.dev@Gmail.com>
2020-05-22 19:26:19 -0500

    Make initializing the Dazor schema more robust; will now create database if it doesn't exist, and wait till the database is ready to accept connections (for 30 seconds).

--- Dazor/Cli/Commands/InitCommand.cs
+++ Dazor/Cli/Commands/InitCommand.cs
@@ -4,6 +4,9 @@ using Dazor.Cli.Options;
 using Microsoft.Data.SqlClient;
 using Dapper;
 using System.IO;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Dazor.Cli.Commands {
   internal class InitCommand : ICommand {
@@ -19,7 +22,49 @@ namespace Dazor.Cli.Commands {
     }
 
     private async Task InitDazorSchemaAsync() {
-      using var connection = new SqlConnection(_options.ConnectionString);
+      try {
+        await TryInitDazorSchemaAsync(_options.ConnectionString);
+      } catch (SqlException) {
+        var builder = new SqlConnectionStringBuilder(_options.ConnectionString);
+        var desiredDatabaseName = builder.InitialCatalog;
+        builder.InitialCatalog = "master";
+        var masterConnectionString = builder.ToString();
+        await TryCreateDbAsync(masterConnectionString, desiredDatabaseName);
+        await WaitForDatabaseReadyAsync(_options.ConnectionString);
+        await TryInitDazorSchemaAsync(_options.ConnectionString);
+      }
+    }
+
+    private async Task TryCreateDbAsync(string connectionString, string databaseName) {
+      using var connection = new SqlConnection(connectionString);
+      await connection.OpenAsync();
+      await connection.ExecuteAsync($@"CREATE DATABASE [{databaseName}];");
+    }
+
+    private async Task WaitForDatabaseReadyAsync(string connectionString) {
+      using var connection = new SqlConnection(connectionString);
+      var stopwatch = new Stopwatch();
+      var timeout = TimeSpan.FromSeconds(30);
+      var delay = 100;
+      while (true) {
+        try {
+          if (stopwatch.Elapsed >= timeout) throw new TimeoutException("Could not login to database after creating it.");
+          await connection.OpenAsync();
+          await connection.CloseAsync();
+          break;
+        } catch (SqlException ex) {
+          // https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008-r2/cc645613%28v%3dsql.105%29
+          // Cannot open database "%.*ls" requested by the login. The login failed.
+          if (ex.Number != 4060) throw;
+          if (stopwatch.Elapsed >= timeout) throw;
+          await Task.Delay(delay);
+          delay = Math.Min(400, delay + 100);
+        }
+      }
+    }
+
+    private async Task TryInitDazorSchemaAsync(string connectionString) {
+      using var connection = new SqlConnection(connectionString);
       await connection.OpenAsync();
       using var transaction = connection.BeginTransaction();
       // TODO: This needs to be built into the app or found relative to the deployment.
