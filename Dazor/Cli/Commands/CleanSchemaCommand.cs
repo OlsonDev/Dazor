0771312d0bf9605ce9f627f1cc5f0ab474f30c6b
Josh Olson <Olson.dev@Gmail.com>
2020-06-03 08:46:00 -0500

    `+=` is a thing.

--- Dazor/Cli/Commands/CleanSchemaCommand.cs
+++ Dazor/Cli/Commands/CleanSchemaCommand.cs
@@ -14,44 +14,44 @@ namespace Dazor.Cli.Commands {
         DECLARE @DazorSchemaID INT = ISNULL((SELECT SCHEMA_ID('Dazor')), (SELECT SCHEMA_ID('sys')));
         DECLARE @SQL NVARCHAR(MAX) = N'';
 
-        SET @SQL = @SQL + '-- Procedures';
+        SET @SQL += '-- Procedures';
         SELECT @SQL = @SQL + @NewLine + 'DROP PROCEDURE [' + SCHEMA_NAME(P.Schema_ID) + '].[' + P.Name + '];'
         FROM sys.Procedures AS P
         WHERE P.Schema_ID != @DazorSchemaID;
 
-        SET @SQL = @SQL + @NewLine + '-- Check constraints';
+        SET @SQL += @NewLine + '-- Check constraints';
         SELECT @SQL = @SQL + @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(CC.Schema_ID) + '].[' + OBJECT_NAME(CC.Parent_Object_ID) + ']' + @NewLine + '  DROP CONSTRAINT ' + CC.Name + '];'
         FROM sys.Check_Constraints AS CC
         WHERE CC.Schema_ID != @DazorSchemaID;
 
-        SET @SQL = @SQL + @NewLine + '-- Functions';
+        SET @SQL += @NewLine + '-- Functions';
         SELECT @SQL = @SQL + @NewLine + 'DROP FUNCTION [' + SCHEMA_NAME(F.Schema_ID) + '].[' + F.Name + '];'
         FROM sys.Objects AS F
         WHERE F.Schema_ID != @DazorSchemaID
           AND F.Type IN ('FN', 'IF', 'TF');
 
-        SET @SQL = @SQL + @NewLine + '-- Views';
+        SET @SQL += @NewLine + '-- Views';
         SELECT @SQL = @SQL + @NewLine + 'DROP VIEW [' + SCHEMA_NAME(V.Schema_ID) + '].[' + V.Name + '];'
         FROM sys.Views AS V
         WHERE V.Schema_ID != @DazorSchemaID;
 
-        SET @SQL = @SQL + @NewLine + '-- Foreign keys';
+        SET @SQL += @NewLine + '-- Foreign keys';
         SELECT @SQL = @SQL + @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(FK.Schema_ID) + '].[' + OBJECT_NAME(FK.Parent_Object_ID ) + ']' + @NewLine + '  DROP CONSTRAINT [' + FK.Name + '];'
         FROM sys.Foreign_Keys AS FK
         WHERE FK.Schema_ID != @DazorSchemaID;
 
-        SET @SQL = @SQL + @NewLine + '-- Temporal (system-versioned) tables';
+        SET @SQL += @NewLine + '-- Temporal (system-versioned) tables';
         SELECT @SQL = @SQL + @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + ']' + @NewLine + '  SET (SYSTEM_VERSIONING = OFF);'
         FROM sys.Tables AS T
         WHERE T.Schema_ID != @DazorSchemaID
           AND T.Temporal_Type = 2;
 
-        SET @SQL = @SQL + @NewLine + '-- Tables';
+        SET @SQL += @NewLine + '-- Tables';
         SELECT @SQL = @SQL + @NewLine + 'DROP TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
         FROM sys.Tables AS T
         WHERE T.Schema_ID != @DazorSchemaID;
 
-        SET @SQL = @SQL + @NewLine + '-- User Defined Types (UDTs)';
+        SET @SQL += @NewLine + '-- User Defined Types (UDTs)';
         SELECT @SQL = @SQL + @NewLine + 'DROP TYPE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
         FROM sys.Types AS T
         WHERE T.Schema_ID != @DazorSchemaID
