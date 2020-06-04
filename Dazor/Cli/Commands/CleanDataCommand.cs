78fe05bf902dbab5fa865e7b18b335a09dc4bb16
Josh Olson <Olson.dev@Gmail.com>
2020-06-03 19:01:57 -0500

    Whitespace

--- Dazor/Cli/Commands/CleanDataCommand.cs
+++ Dazor/Cli/Commands/CleanDataCommand.cs
@@ -11,84 +11,84 @@ namespace Dazor.Cli.Commands {
     private readonly BoundConfig _config;
 
     private static readonly string BuildDynamicCleanCommand = @"
-        DECLARE @ExcludedSchemaIDs TABLE (SchemaID INT NOT NULL PRIMARY KEY);
-        INSERT @ExcludedSchemaIDs
-        SELECT S.Schema_ID
+      DECLARE @ExcludedSchemaIDs TABLE (SchemaID INT NOT NULL PRIMARY KEY);
+      INSERT @ExcludedSchemaIDs
+      SELECT S.Schema_ID
+      FROM (SELECT _ = NULL) AS _
+      JOIN sys.Schemas       AS S  ON S.Name IN ('Dazor', 'sys', 'guest', 'INFORMATION_SCHEMA') OR Schema_ID >= 16384;
+
+
+      DECLARE @SQL NVARCHAR(MAX) = N'SET NOCOUNT ON;';
+
+
+      SET @SQL += @NewLine + '-- Disable triggers';
+      WITH DistinctTablesWithTriggers AS (
+        SELECT DISTINCT
+            SchemaName = SCHEMA_NAME(O.Schema_ID)
+          , TableName = O.Name
         FROM (SELECT _ = NULL) AS _
-        JOIN sys.Schemas       AS S  ON S.Name IN ('Dazor', 'sys', 'guest', 'INFORMATION_SCHEMA') OR Schema_ID >= 16384;
+        JOIN sys.Triggers AS T ON T.Parent_Class = 1 -- Object or Column
+        JOIN sys.Objects  AS O ON O.Object_ID = T.Parent_ID
+        WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      )
+      SELECT @SQL += @NewLine + 'DISABLE TRIGGER ALL ON [' + T.SchemaName + '].[' + T.TableName + '];'
+      FROM DistinctTablesWithTriggers AS T
+      ORDER BY T.SchemaName, T.TableName;
+
+      SET @SQL += @NewLine + '-- Disable constraints';
+      WITH DistinctTablesWithConstraints AS (
+        SELECT DISTINCT
+            SchemaName = SCHEMA_NAME(O.Schema_ID)
+          , TableName = O.Name
+        FROM sys.Foreign_Keys AS FK
+        JOIN sys.Objects      AS O ON O.Object_ID = FK.Parent_Object_ID
+        WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      )
+      SELECT @SQL += @NewLine + 'ALTER TABLE [' + T.SchemaName + '].[' + T.TableName + '] NOCHECK CONSTRAINT ALL;'
+      FROM DistinctTablesWithConstraints AS T
+      ORDER BY T.SchemaName, T.TableName;
+
+      SET @SQL += @NewLine + '-- Delete data';
+      SELECT @SQL += @NewLine + 'DELETE FROM [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
+      FROM sys.Tables AS T
+      WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      ORDER BY SCHEMA_NAME(T.Schema_ID), T.Name;
 
 
-        DECLARE @SQL NVARCHAR(MAX) = N'SET NOCOUNT ON;';
+      SET @SQL += @NewLine + '-- Reenable constraints';
+      WITH DistinctTablesWithConstraints AS (
+        SELECT DISTINCT
+            SchemaName = SCHEMA_NAME(O.Schema_ID)
+          , TableName = O.Name
+        FROM sys.Foreign_Keys AS FK
+        JOIN sys.Objects      AS O ON O.Object_ID = FK.Parent_Object_ID
+        WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      )
+      SELECT @SQL += @NewLine + 'ALTER TABLE [' + T.SchemaName + '].[' + T.TableName + '] CHECK CONSTRAINT ALL;'
+      FROM DistinctTablesWithConstraints AS T
+      ORDER BY T.SchemaName, T.TableName;
 
 
-        SET @SQL += @NewLine + '-- Disable triggers';
-        WITH DistinctTablesWithTriggers AS (
-          SELECT DISTINCT
-              SchemaName = SCHEMA_NAME(O.Schema_ID)
-            , TableName = O.Name
-          FROM (SELECT _ = NULL) AS _
-          JOIN sys.Triggers AS T ON T.Parent_Class = 1 -- Object or Column
-          JOIN sys.Objects  AS O ON O.Object_ID = T.Parent_ID
-          WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        )
-        SELECT @SQL += @NewLine + 'DISABLE TRIGGER ALL ON [' + T.SchemaName + '].[' + T.TableName + '];'
-        FROM DistinctTablesWithTriggers AS T
-        ORDER BY T.SchemaName, T.TableName;
-
-        SET @SQL += @NewLine + '-- Disable constraints';
-        WITH DistinctTablesWithConstraints AS (
-          SELECT DISTINCT
-              SchemaName = SCHEMA_NAME(O.Schema_ID)
-            , TableName = O.Name
-          FROM sys.Foreign_Keys AS FK
-          JOIN sys.Objects      AS O ON O.Object_ID = FK.Parent_Object_ID
-          WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        )
-        SELECT @SQL += @NewLine + 'ALTER TABLE [' + T.SchemaName + '].[' + T.TableName + '] NOCHECK CONSTRAINT ALL;'
-        FROM DistinctTablesWithConstraints AS T
-        ORDER BY T.SchemaName, T.TableName;
-
-        SET @SQL += @NewLine + '-- Delete data';
-        SELECT @SQL += @NewLine + 'DELETE FROM [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
-        FROM sys.Tables AS T
-        WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        ORDER BY SCHEMA_NAME(T.Schema_ID), T.Name;
+      SET @SQL += @NewLine + '-- Reenable triggers';
+      WITH DistinctTablesWithTriggers AS (
+        SELECT DISTINCT
+            SchemaName = SCHEMA_NAME(O.Schema_ID)
+          , TableName = O.Name
+        FROM (SELECT _ = NULL) AS _
+        JOIN sys.Triggers AS T ON T.Parent_Class = 1 -- Object or Column
+        JOIN sys.Objects  AS O ON O.Object_ID = T.Parent_ID
+        WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      )
+      SELECT @SQL += @NewLine + 'ENABLE TRIGGER ALL ON [' + T.SchemaName + '].[' + T.TableName + '];'
+      FROM DistinctTablesWithTriggers AS T
+      ORDER BY T.SchemaName, T.TableName;
 
 
-        SET @SQL += @NewLine + '-- Reenable constraints';
-        WITH DistinctTablesWithConstraints AS (
-          SELECT DISTINCT
-              SchemaName = SCHEMA_NAME(O.Schema_ID)
-            , TableName = O.Name
-          FROM sys.Foreign_Keys AS FK
-          JOIN sys.Objects      AS O ON O.Object_ID = FK.Parent_Object_ID
-          WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        )
-        SELECT @SQL += @NewLine + 'ALTER TABLE [' + T.SchemaName + '].[' + T.TableName + '] CHECK CONSTRAINT ALL;'
-        FROM DistinctTablesWithConstraints AS T
-        ORDER BY T.SchemaName, T.TableName;
-
-
-        SET @SQL += @NewLine + '-- Reenable triggers';
-        WITH DistinctTablesWithTriggers AS (
-          SELECT DISTINCT
-              SchemaName = SCHEMA_NAME(O.Schema_ID)
-            , TableName = O.Name
-          FROM (SELECT _ = NULL) AS _
-          JOIN sys.Triggers AS T ON T.Parent_Class = 1 -- Object or Column
-          JOIN sys.Objects  AS O ON O.Object_ID = T.Parent_ID
-          WHERE O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        )
-        SELECT @SQL += @NewLine + 'ENABLE TRIGGER ALL ON [' + T.SchemaName + '].[' + T.TableName + '];'
-        FROM DistinctTablesWithTriggers AS T
-        ORDER BY T.SchemaName, T.TableName;
-
-
-        SET @SQL += @NewLine + '-- Reseed identities which have had values before; avoid reseeding identities of tables which have never had records (since truncation)';
-        SELECT @SQL += @NewLine + 'DBCC CHECKIDENT (''[' + SCHEMA_NAME(O.Schema_ID) + '].[' + O.Name + ']'', RESEED, ' + CONVERT(NVARCHAR(20), CONVERT(BIGINT, IC.Seed_Value) - 1) + ') WITH NO_INFOMSGS;'
-        FROM sys.Identity_Columns AS IC
-        JOIN sys.Objects          AS O   ON IC.Object_ID = O.Object_ID AND O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-        ORDER BY SCHEMA_NAME(O.Schema_ID), O.Name;";
+      SET @SQL += @NewLine + '-- Reseed identities which have had values before; avoid reseeding identities of tables which have never had records (since truncation)';
+      SELECT @SQL += @NewLine + 'DBCC CHECKIDENT (''[' + SCHEMA_NAME(O.Schema_ID) + '].[' + O.Name + ']'', RESEED, ' + CONVERT(NVARCHAR(20), CONVERT(BIGINT, IC.Seed_Value) - 1) + ') WITH NO_INFOMSGS;'
+      FROM sys.Identity_Columns AS IC
+      JOIN sys.Objects          AS O   ON IC.Object_ID = O.Object_ID AND O.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+      ORDER BY SCHEMA_NAME(O.Schema_ID), O.Name;";
 
     private static readonly string GetCleanCommand = BuildDynamicCleanCommand + Environment.NewLine + Environment.NewLine + "SELECT @SQL;";
     private static readonly string ExecuteCleanCommand = BuildDynamicCleanCommand + Environment.NewLine + Environment.NewLine + "EXEC SP_ExecuteSQL @SQL;";
--- Dazor/Cli/Commands/CleanSchemaCommand.cs
+++ Dazor/Cli/Commands/CleanSchemaCommand.cs
@@ -11,64 +11,64 @@ namespace Dazor.Cli.Commands {
     private readonly BoundConfig _config;
 
     private static readonly string BuildDynamicCleanCommand = @"
-        DECLARE @ExcludedSchemaIDs TABLE (SchemaID INT NOT NULL PRIMARY KEY);
-        INSERT @ExcludedSchemaIDs
-        SELECT S.Schema_ID
-        FROM (SELECT _ = NULL) AS _
-        JOIN sys.Schemas       AS S  ON S.Name IN ('Dazor', 'sys', 'guest', 'INFORMATION_SCHEMA') OR Schema_ID >= 16384;
+      DECLARE @ExcludedSchemaIDs TABLE (SchemaID INT NOT NULL PRIMARY KEY);
+      INSERT @ExcludedSchemaIDs
+      SELECT S.Schema_ID
+      FROM (SELECT _ = NULL) AS _
+      JOIN sys.Schemas       AS S  ON S.Name IN ('Dazor', 'sys', 'guest', 'INFORMATION_SCHEMA') OR Schema_ID >= 16384;
 
 
-        DECLARE @SQL NVARCHAR(MAX) = N'';
+      DECLARE @SQL NVARCHAR(MAX) = N'';
 
 
-        SET @SQL += '-- Procedures';
-        SELECT @SQL += @NewLine + 'DROP PROCEDURE [' + SCHEMA_NAME(P.Schema_ID) + '].[' + P.Name + '];'
-        FROM sys.Procedures AS P
-        WHERE P.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
+      SET @SQL += '-- Procedures';
+      SELECT @SQL += @NewLine + 'DROP PROCEDURE [' + SCHEMA_NAME(P.Schema_ID) + '].[' + P.Name + '];'
+      FROM sys.Procedures AS P
+      WHERE P.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
 
 
-        SET @SQL += @NewLine + '-- Check constraints';
-        SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(CC.Schema_ID) + '].[' + OBJECT_NAME(CC.Parent_Object_ID) + ']' + @NewLine + '  DROP CONSTRAINT ' + CC.Name + '];'
-        FROM sys.Check_Constraints AS CC
-        WHERE CC.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
+      SET @SQL += @NewLine + '-- Check constraints';
+      SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(CC.Schema_ID) + '].[' + OBJECT_NAME(CC.Parent_Object_ID) + ']' + @NewLine + '  DROP CONSTRAINT ' + CC.Name + '];'
+      FROM sys.Check_Constraints AS CC
+      WHERE CC.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
 
 
-        SET @SQL += @NewLine + '-- Functions';
-        SELECT @SQL += @NewLine + 'DROP FUNCTION [' + SCHEMA_NAME(F.Schema_ID) + '].[' + F.Name + '];'
-        FROM sys.Objects AS F
-        WHERE F.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-          AND F.Type IN ('FN', 'IF', 'TF');
+      SET @SQL += @NewLine + '-- Functions';
+      SELECT @SQL += @NewLine + 'DROP FUNCTION [' + SCHEMA_NAME(F.Schema_ID) + '].[' + F.Name + '];'
+      FROM sys.Objects AS F
+      WHERE F.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+        AND F.Type IN ('FN', 'IF', 'TF');
 
 
-        SET @SQL += @NewLine + '-- Views';
-        SELECT @SQL += @NewLine + 'DROP VIEW [' + SCHEMA_NAME(V.Schema_ID) + '].[' + V.Name + '];'
-        FROM sys.Views AS V
-        WHERE V.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
+      SET @SQL += @NewLine + '-- Views';
+      SELECT @SQL += @NewLine + 'DROP VIEW [' + SCHEMA_NAME(V.Schema_ID) + '].[' + V.Name + '];'
+      FROM sys.Views AS V
+      WHERE V.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
 
-        SET @SQL += @NewLine + '-- Foreign keys';
-        SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(FK.Schema_ID) + '].[' + OBJECT_NAME(FK.Parent_Object_ID ) + ']' + @NewLine + '  DROP CONSTRAINT [' + FK.Name + '];'
-        FROM sys.Foreign_Keys AS FK
-        WHERE FK.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
-
-
-        SET @SQL += @NewLine + '-- Temporal (system-versioned) tables';
-        SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + ']' + @NewLine + '  SET (SYSTEM_VERSIONING = OFF);'
-        FROM sys.Tables AS T
-        WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-          AND T.Temporal_Type = 2;
+      SET @SQL += @NewLine + '-- Foreign keys';
+      SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(FK.Schema_ID) + '].[' + OBJECT_NAME(FK.Parent_Object_ID ) + ']' + @NewLine + '  DROP CONSTRAINT [' + FK.Name + '];'
+      FROM sys.Foreign_Keys AS FK
+      WHERE FK.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
 
 
-        SET @SQL += @NewLine + '-- Tables';
-        SELECT @SQL += @NewLine + 'DROP TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
-        FROM sys.Tables AS T
-        WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
+      SET @SQL += @NewLine + '-- Temporal (system-versioned) tables';
+      SELECT @SQL += @NewLine + 'ALTER TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + ']' + @NewLine + '  SET (SYSTEM_VERSIONING = OFF);'
+      FROM sys.Tables AS T
+      WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+        AND T.Temporal_Type = 2;
 
 
-        SET @SQL += @NewLine + '-- User Defined Types (UDTs)';
-        SELECT @SQL += @NewLine + 'DROP TYPE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
-        FROM sys.Types AS T
-        WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
-          AND T.Is_User_Defined = 1;";
+      SET @SQL += @NewLine + '-- Tables';
+      SELECT @SQL += @NewLine + 'DROP TABLE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
+      FROM sys.Tables AS T
+      WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs);
+
+
+      SET @SQL += @NewLine + '-- User Defined Types (UDTs)';
+      SELECT @SQL += @NewLine + 'DROP TYPE [' + SCHEMA_NAME(T.Schema_ID) + '].[' + T.Name + '];'
+      FROM sys.Types AS T
+      WHERE T.Schema_ID NOT IN (SELECT SchemaID FROM @ExcludedSchemaIDs)
+        AND T.Is_User_Defined = 1;";
 
     private static readonly string GetCleanCommand = BuildDynamicCleanCommand + Environment.NewLine + Environment.NewLine + "SELECT @SQL;";
     private static readonly string ExecuteCleanCommand = BuildDynamicCleanCommand + Environment.NewLine + Environment.NewLine + "EXEC SP_ExecuteSQL @SQL;";
